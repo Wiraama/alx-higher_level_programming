ad19ded2d48ec12406165b5cbe7a78ffb8fe763e
Wiraama <Wiraama>
2024-03-14 04:44:38 -0700

    mawira work

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-massage = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(massage)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
 sts.exit(1)
