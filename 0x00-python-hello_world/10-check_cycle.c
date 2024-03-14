f8c6eced68543a731ae168e80093409e36ee2f16
Wiraama <Wiraama>
2024-03-14 04:42:14 -0700

    mawira work

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,14 +1,29 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * check_cycle(listint_t *list) - main entry point
+ * @list: ...
+ *
+ * Return: 1
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list;
-	while (current)
+	listint_t *slow, *fast;
+
+	if (!list)
 	{
-		current = current->next;
-		if ( current == list)
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)while (fast && slow && fast->next)
+	{
+		if (slow == fast)
 		{
 			return (1);
 		}
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
