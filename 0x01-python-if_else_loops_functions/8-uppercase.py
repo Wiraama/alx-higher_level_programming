de583bbc575c417b4f2aa9685c0cf820d21841aa
Wiraama <Wiraama>
2024-03-15 02:50:44 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,12 @@
-#!/usr/binn/python3
+#!/usr/bin/python3
 '''fuction to print string in uppercase'''
 
 
 def uppercase(str):
     for s in str:
         if 'a' <= s <= 'z':
-            '''mean is a lowercase cahracter'''
-            uppers = chr(ord(s) - 32)
+            S = chr(ord(s) - 32)
         else:
-            uppers = s
-        print(uppers, end="")
+            S = s
+        print("{:c}".format(S), end="")
+    return S
