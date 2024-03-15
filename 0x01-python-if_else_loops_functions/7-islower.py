5406130d9e4f3825a20bf6ec0d3dfd76e4720843
Wiraama <Wiraama>
2024-03-15 01:35:00 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 ''' fuction to cheack for lower case letter'''
 def islower(c):
-    if c == range(97, 123):
+    if ord(c)== range(97, 123):
         return (True)
     else:
         return (False)
