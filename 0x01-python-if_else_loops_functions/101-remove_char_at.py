d6f454ac3a4fcd14a75f7ec41b4cfc9f042ed261
Wiraama <Wiraama>
2024-03-16 10:36:36 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-'''Write a function that creates a copy of the string, removing the character at the position n '''
+'''Write a function that creates
+a copy of the string, removing the character at the position n '''
 
 
 def remove_char_at(str, n):
+    if n < 0 or n > len(str):
+        return str
     return str[:n] + str[n + 1:]
