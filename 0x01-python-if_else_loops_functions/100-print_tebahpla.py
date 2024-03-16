dbf7fe37425d830b5e4232fa3ebb9418e18fe882
Wiraama <Wiraama>
2024-03-16 09:28:25 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(ord('z'), ord('a') -1, -1):
     if letter % 2 == 0:
-        print(chr(letter), end='')
+        print("{}".format(chr(letter)), end='')
     else:
-        print(chr(letter - 32), end='')
+        print("{}".format(chr(letter - 32)), end='')
