91105a0da8a61c483868b047fb95236e1d4c0d71
Wiraama <Wiraama>
2024-03-16 09:32:01 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for letter in range(ord('z'), ord('a') -1, -1):
+'''alphabet in reverse order an in uppercase and lower case in a manner '''
+
+
+for letter in range(ord('z'), ord('a') -1, -1 ):
     if letter % 2 == 0:
         print("{}".format(chr(letter)), end='')
     else:
