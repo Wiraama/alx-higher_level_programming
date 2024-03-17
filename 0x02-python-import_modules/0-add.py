92cd0da7a3d4133b04906292d767cc32c3de183e
Wiraama <Wiraama>
2024-03-16 18:13:53 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+'''mports the function def add(a, b): from the file add_0.py and prints the result of the addition'''
+
+
+a = 1
+b = 2
+from add_0 import add
+c = add(a, b)
+print("{} + {} = {}".format(a, b, c))
