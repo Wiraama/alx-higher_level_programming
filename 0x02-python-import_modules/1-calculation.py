e54e1fdc2dbf1e95c8779172fac2dbc2e5c93e8a
Wiraama <Wiraama>
2024-03-17 13:10:16 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''program that import function frm the file , does some math and print results'''
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     from calculator_1 import sub, add, mul, div
     a = 10
     b = 5
-    print(f"{a:d} - {b:d} = {sub(a, b)}")
-    print(f"{a:d} + {b:d} = {add(a, b)}")
-    print(f"{a:d} * {b:d} = {mul(a, b)}i")
-    print(f"{a:d} / {b:d} = {div(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}i")
+    print(f"{a} / {b} = {div(a, b)}")
