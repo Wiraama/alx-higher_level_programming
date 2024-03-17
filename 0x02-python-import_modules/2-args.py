7ed015efcc63db0c42b81f01874dc474d32bd8a0
Wiraama <Wiraama>
2024-03-17 15:59:22 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
 
     if args_len == 0:
         print("0 Arguments.")
+    elif args_len == 1:
+        print("1 argument")
     else:
         print("{} arguments:".format(args_len))
-        for i, args in enumerate(sys.argv[1:], 1):
-            print("{}: {}".format(i, args))
+    for i, args in enumerate(sys.argv[1:], 1):
+        print("{}: {}".format(i, args))
