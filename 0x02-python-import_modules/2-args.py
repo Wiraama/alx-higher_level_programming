d50634095267454e6f45664f0771d5aa5ea03890
Wiraama <Wiraama>
2024-03-17 16:13:55 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,9 @@
 if __name__ == "__main__":
     """..."""
 
-
     import sys
 
-
-    args = len(sys.argv) -1
+    args = len(sys.argv) - 1
     if args == 0:
         print("0 arguments.")
     elif args == 1:
