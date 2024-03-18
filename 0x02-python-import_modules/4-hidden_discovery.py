7481defda09bc4fb0120d163495eea607aa4bb57
Wiraama <Wiraama>
2024-03-18 03:29:54 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Write a program that prints all the names defined by the compiled module hidden_4.pyc"""
+""" Write a program that prints all the
+names defined by the compiled module hidden_4.pyc"""
 
 
 if __name__ == "__main__":
     import hidden_4
-
 
     name = dir(hidden_4)
     for n in name:
