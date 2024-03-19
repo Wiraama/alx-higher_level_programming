3614728411cf5c3ec19a851cbb0d7b750c926319
Wiraama <Wiraama>
2024-03-19 03:59:15 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return None
-    else:
-        return max(my_list)
+    number = my_list[0]
+    for num in my_list:
+        if num > number:
+            number = num
+    return number
