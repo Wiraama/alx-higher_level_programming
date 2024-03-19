7ce1c6f11ca8cdbbd744d0b55817457306d4b99c
Wiraama <Wiraama>
2024-03-19 13:48:48 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    t = ()
-    count = 0
-    if sentence == 0:
-        t = (0, "None")
-
-    else:
-        count = len(sentence)
-        first = sentence[0]
-        t = (count, first)
-    return t
+    total = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new = total, char
+    return(new)
