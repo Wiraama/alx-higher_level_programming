ecaa86cb59b1111c93c8b0a199da877518ecfa71
Wiraama <Wiraama>
2024-03-19 03:16:26 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     count = 0
+    if sentence == 0:
+        first = None
+        return count, first
+
     count = len(sentence)
     first = sentence[0]
     return count, first
