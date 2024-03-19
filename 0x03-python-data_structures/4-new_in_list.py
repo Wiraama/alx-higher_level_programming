fcded2687d4ad9c285573dab7ee5035ec78ee0f3
Wiraama <Wiraama>
2024-03-19 02:11:51 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return None
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
     else:
+        '''making a copy'''
         new_list = my_list[:]
         new_list[idx] = element
         return new_list
