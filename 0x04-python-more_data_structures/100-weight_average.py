bec6fde7a465b91cb6caf9956a85b47c472243a1
Wiraama <Wiraama>
2024-03-22 15:08:24 -0700

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    number = 0
+    index = 0
+
+    for t in my_list:
+        number += tup[0] * tup[1]
+        index += tup[1]
+
+        return (number / index)
