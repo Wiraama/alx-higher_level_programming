b270259f110552775ef93c79852a4c7d5e8c6400
Wiraama <Wiraama>
2024-03-22 11:59:27 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
     highest = None
     best = None
     for key, value in a_dictionary.items():
-        current = max(value)
-        if highest is None or current > highest:
-            highest = current
+
+        if highest is None or value > highest:
+            highest = value
             best = key
-    return best, highest
+    return best
