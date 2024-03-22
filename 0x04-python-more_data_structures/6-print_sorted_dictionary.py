93ee263b6631a09f912e5568905418e9c7a0d8a0
Wiraama <Wiraama>
2024-03-22 05:56:46 -0700

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     a_dict = sorted(a_dictionary.keys())
     for a in a_dict:
-        print(a, ":", a_dictionary[a])
+        print(a + ":", a_dictionary[a])
