5fc5ab779e0286087d87ec5ea0f13d39eb4cc4be
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-08 19:16:38 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
         """creates new instances"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size <= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
