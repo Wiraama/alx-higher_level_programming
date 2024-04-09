173e53518f77e1ddc6d93b3e1b26057dde241919
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-08 18:20:43 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-"""class Square that defines a square by: (based on 1-square.py)"""
+"""defines sa class"""
 
 
 class Square:
-    """class defination"""
+    """reps square"""
+
     def __init__(self, size=0):
-        try:
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        except TypeError as e:
-            print (e)
-        except ValueError as e:
-            print (e)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
