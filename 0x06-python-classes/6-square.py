8a502439ad4dc493276d9882dc2471710c0fe3db
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-09 12:23:39 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
         def size(self, value):
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-                elif value < 0:
+            elif value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
@@ -43,12 +43,12 @@ class Square:
 
                 def my_print(self):
                 """Print the square with the # character."""
-                if self.__size == 0:
-                print("")
-                return
+                    if self.__size == 0:
+                        print("")
+                        return
 
-                [print("") for i in range(0, self.__position[1])]
-                for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+                    [print("") for i in range(0, self.__position[1])]
+                    for i in range(0, self.__size):
+                        [print(" ", end="") for j in range(0, self.__position[0])]
+                        [print("#", end="") for k in range(0, self.__size)]
+                        print("")
