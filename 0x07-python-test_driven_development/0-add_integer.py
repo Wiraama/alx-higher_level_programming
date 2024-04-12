e715b4711578e128d5229cd6e53d26c4724abd73
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-12 06:48:12 -0700

    0-add_integer.py,

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,16 @@
 #!/bin/python3
+"""
+    rototype: def add_integer(a, b=98):
+    a and b must be integers or floats,
+    otherwise raise a TypeError exception with the message a must be an 
+    integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    You are not allowed to import any module
+"""
 
 def add_integer(a, b=98):
     """unction that adds 2 integers."""
-
 
     if type(a) is not int and type(a) is not float:
         """you can use if not isinstance(a, (int, float)):
