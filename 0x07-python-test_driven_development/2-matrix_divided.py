6cc5629b0e196f9ba8f7caf6ab6c82d1c6b845fd
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-13 15:38:57 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-
 def matrix_divided(matrix, div):
-    """ function that divides all elements of a matrix. """
-    if type(div) is not int and type(div) is not float:
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(errorMessage)
+    if not isinstance(matrix, list):
+        raise TypeError(errorMessage)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(errorMessage)
+        for item in lists:
+            if not isinstance(item, int) and not isinstance(item, float):
+                raise TypeError(errorMessage)
+    for lists in matrix:
+        if len(lists) == 0:
+            raise TypeError(errorMessage)
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    elif div == 0:
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    row_size = len(matrix[0])
-
-    new_matrix = []
-    for row in matrix:
-        results = []
-        if len(row) != row_size:
-            raise TypeError("Each row of the matrix must have the same size")
-        for element in row:
-            if type(element) is not int and type(element) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                result = element / div
-                result = f"{result:.2f}"
-                results.append(result)
-        new_matrix.append(results)
-    return new_matrix
+    return [[round(item / div, 2) for item in lists] for lists in matrix]
