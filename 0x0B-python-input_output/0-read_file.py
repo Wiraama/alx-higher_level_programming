3a4fd7fa38b4dc12cc62ff93746ddf52c8a128df
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-17 13:48:34 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""unction that reads a text file (UTF8) and prints it to stdout:"""
+
 
 def read_file(filename=""):
+    """Printing content in a file"""
     with open(filename, mode='r', encoding=("utf-8")) as f:
         myfile = f.read()
-        print(myfile)
+        print(myfile, end="")
