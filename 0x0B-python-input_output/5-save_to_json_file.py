88dc98cd65f707df2bbecd5471984e843e72bb0f
Wiraama <Wiraama>
2024-04-17 16:41:53 -0700

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""unction that writes an Object to a text file, using a JSON representation:"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """returns file written from object"""
+
+    with open(filename, 'w', encoding="utf-8")) as f:
+        json.dump(my_obj, f)
