8a0be73443480b6a2450f6e0e463ec4652e0a880
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-17 14:06:53 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file 
+(UTF8) and returns the number of characters written:"""
+
+
+def write_file(filename="", text=""):
+    """writting to a file"""
+
+    with open(filename, mode='w', encoding=("utf-8")) as f:
+        my_file = f.write(text)
+        return(my_file)
