7fe05d50ed0d300e8d7e26179618de36f34be787
Wiraama <Wiraama>
2024-04-17 16:10:06 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function that returns an object (Python data structure)
+represented by a JSON string:"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    new_file = json.loads(my_str)
+    return new_file
