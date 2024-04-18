786cc2c8334caa341c06f0267f00a7d2f16ca7e0
Wiraama <Wiraama>
2024-04-17 18:02:50 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""">>>>>>>>>>"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
     """............................."""
 
+    texts = ""
     with open(filename) as r:
         for line in r:
             text += line
