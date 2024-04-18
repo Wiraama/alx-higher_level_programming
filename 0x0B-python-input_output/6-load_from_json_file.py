6b1c2249500cc552a3253934272b4790825d5df7
Wiraama <Wiraama>
2024-04-17 17:17:06 -0700

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """creating object from json"""
 
-    with open(filename, 'w', encoding="utf-8") as f:
-        return json.loads(f)
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
