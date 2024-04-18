a289d09d0b10c6c303962afbaf6a13a88de47f1c
Wiraama <Wiraama>
2024-04-17 17:08:54 -0700

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that creates an Object from a “JSON file”:"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creating object from json"""
+
+    with open(filename, 'w') as f:
+        json.load(f)
