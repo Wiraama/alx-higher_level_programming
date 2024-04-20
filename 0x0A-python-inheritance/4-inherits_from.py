87645d3b0c8a2d90e5dab785f27b93edab63c983
Wiraama <Wiraama>
2024-04-20 11:42:34 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module to return if object is intancev"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False. """
+
+    return type(obj) is not a_class
