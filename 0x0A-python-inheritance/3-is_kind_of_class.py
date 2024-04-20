c4ab8d0cc002adc7283edb4b3ade0f19b72ae99a
Wiraama <Wiraama>
2024-04-20 11:27:01 -0700

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module to this fuction """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    if object is an instance/insatnce of a class return True
+    else false
+    """
+
+    return isinstance(obj, a_class)
