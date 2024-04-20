eba04455ed42bf8967616f26d9a909733508352b
Wiraama <Wiraama>
2024-04-20 13:06:48 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module to host """
+
+
+class BaseGeometry:
+    """ class baseGeomrtry """
+
+    def area(self):
+        """ public instance to raise errror """
+
+        raise Exception("area() is not implemented")
