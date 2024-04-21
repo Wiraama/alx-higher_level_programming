25dc2eb44ef40d2095dcf0dd944f04b02d4ea8df
Wiraama <Wiraama>
2024-04-21 01:42:40 -0700

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""module rectangle """
+
+
+class BaseGeometry:
+    """class base geomentry"""
+    def area(self):
+         raise Exception("area() is not implemented")
+
+     def integer_validator(self, name, value):
+         if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+
+         if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """class recatangle inheriting from basegeometry class"""
+
+    def __init__(self, width, height):
+        """instatiate with width and height
+
+        args:
+            width: as positive int
+            height: as positive int
+        both sould be private
+        """
+
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
