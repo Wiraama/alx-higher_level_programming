1d3ab5aa374882887ddb623844c029e4edcb2048
Wiraama <Wiraama>
2024-04-22 14:27:01 -0700

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """Represent a rectangle."""
 
-    def __init__(self, width, height, x=0, y=0):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
 
         Args:
@@ -21,6 +21,7 @@ class Rectangle(Base):
             TypeError: If either of x or y is not an int.
             ValueError: If either of x or y < 0.
         """
+        super().__init__()
         self.width = width
         self.height = height
         self.x = x
