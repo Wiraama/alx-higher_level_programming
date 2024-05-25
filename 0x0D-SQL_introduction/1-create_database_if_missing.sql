de258b13c42fbe720d4a37003ced9ead77f48ae6
Wiraama <mawira01@students.uonbi.ac.ke>
2024-05-25 05:05:15 -0700

    hbtn_0c_0

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 --script that creates the database hbtn_0c_0 in your MySQL server.--
 --If the database hbtn_0c_0 already exists, your script should not fail--
-CREATE DATABASE IF NOT EXIST 'hbtn_0c_0';
+(IF DATABASE hbtn_0c_0 NOT exist) CREATE DATABASE hbtn_0c_0
