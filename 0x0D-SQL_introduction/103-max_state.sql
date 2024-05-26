f072fa5ce3df060a8fc638e4ae1ba19591f22069
Wiraama <mawira01@students.uonbi.ac.ke>
2024-05-26 08:17:58 -0700

    103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 --  displays the max temperature of each state (ordered by State name).
-SELECT city, MAX(value) AS maw_temp
+SELECT city, MAX(value) AS max_temp
 FROM temperature
 GROUP BY state
 ORDER BY state;
