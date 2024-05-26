ff018bd7e26f7a4720f8788277744320d2740b1e
Wiraama <mawira01@students.uonbi.ac.ke>
2024-05-26 05:14:09 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score >= 10 FROM second_table GROUP BY score ORDER BY DESC;
+SELECT * FROM second_table WHERE score >= 10 GROUP ORDER BY score DESC;
