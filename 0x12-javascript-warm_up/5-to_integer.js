ed0e204ad7b5b55a2ab3c6a09ce5ca73ce5150a0
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-17 17:51:36 -0700

    mae

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,7 +7,7 @@
 const number = process.argv.slice(2);
 const num = parseInt(number[0]);
 
-if (isNaN) {
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + num);
