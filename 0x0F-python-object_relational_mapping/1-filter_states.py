0daed84be07dcbc20db62a16ca332ffc224aaba8
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-21 21:04:08 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" list all states with 'N' in database """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    """ connect to mysql """
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    """ create cursor object """
+    cursor = db.cursor()
+
+    """SELECTING ALL states """
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    states = cursor.fetchall()
+    printed_states = set()
+    for state in states:
+        if state[1] not in printed_states:
+            print(state)
+            printed_states.add(state[1])
+    cursor.close()
+    db.close()
