a81716d5b5a24a00bc0a01872c64d98d84f22ff1
Wiraama <mawira01@students.uonbi.ac.ke>
2024-07-13 00:12:34 -0700

    0-hbtn_status.pyclerr

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    reply = response.read().decode('utf-8')
-    print('Body response')
-    print(f"\t - type: {type(reply)}")
-    print(f"\t - content: {reply}")
-    print(f"\t - utf8 content: {reply}")
+if __name__=="__main__":
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        reply = response.read().decode('utf-8')
+        print('Body response')
+        print(f"\t - type: {type(reply)}")
+        print(f"\t - content: {reply}")
+        print(f"\t - utf8 content: {reply}")
