1d7a3da79d6f4f3adea418c48efe4aa175cd6a4f
mawira <mawira@students.uonbi.ac.ke>
2024-07-31 02:13:53 +0300

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const filename = 'cisfun';
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+	if (err)
+	{
+		console.log("nothing found");
+	}
+	else
+		console.log(data);
+});
