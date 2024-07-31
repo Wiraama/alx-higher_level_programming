18597d964af4a9a6ee081dfca664ce2ae324643c
mawira <mawira@students.uonbi.ac.ke>
2024-07-31 08:17:15 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const content = process.argv[3];
 const filename = process.argv[2];
 
-fs.writeFile(filename, content, 'utf-8', function (err) {
+fs.writeFile(filename, content, 'utf-8', err => {
 	if (err) {
 		console.log(err);
 	}
