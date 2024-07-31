6c8f4ee7359f84f94ef3ed6a93d9884cd5125590
mawira <mawira@students.uonbi.ac.ke>
2024-07-31 09:27:20 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf8', function (err, data) {
-	if (err)
-	{
-		console.log(err);
-	}
-	else {
-		console.log(data);
-	}
+  if (err)
+  {
+    console.log(err);
+  }
+  else {
+    console.log(data);
+  }
 });
