d64ebd53c90b547263625eca1b7ae979e2757620
mawira <mawira@students.uonbi.ac.ke>
2024-07-31 07:46:54 +0300

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const content = process.argv[3];
+const filename = process.argv[2];
+
+fs.writeFile(filename, content, 'utf-8', function (err) {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log(content);
+	}
+});
