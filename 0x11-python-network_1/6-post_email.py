65a851b4de592ce0f07b11ebd4a86938e6f3157e
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-20 01:48:56 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import sys
+import requests
+
 """
 Write a Python script that takes in a URL and an email address,
 sends a POST request to the passed URL with the email as a parameter,
@@ -9,8 +12,6 @@ and finally displays the body of the
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
-import sys
-import requests
 
 if __name__ == '__main__':
     url = sys.argv[1]
