be4f52737f02f1662c11ffb7158cbbf51bbe4b3f
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-20 07:01:37 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    url = "https://api.github.com"
+    url = "https://api.github.com/user"
 
     res = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     if res.status_code == 200:
