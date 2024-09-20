54ef62c347eb27393f74c7da4ac1e1369b55b60d
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-20 07:29:56 +0300

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
     res = requests.get(url)
 
