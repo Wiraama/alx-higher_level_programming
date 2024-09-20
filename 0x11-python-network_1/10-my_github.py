695a16f50389f44a27ef20c388df813c8078de32
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-20 06:59:25 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = "https://api.github.com"
+
+    res = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    if res.status_code == 200:
+        info = res.json()
+        print(info.get("id"))
+    else:
+        print("")
